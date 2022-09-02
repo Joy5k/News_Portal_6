2ff1ffe2239e9225ebee8eccca7c36a6bc355941
Joy <mmehedihasanjoyv@gmail.com>
2022-09-03 00:43:40 +0600

    added title and descriptions

--- app.js
+++ app.js
@@ -39,8 +39,9 @@ const displayLoadData = (allData) => {
         <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
         <img class="  w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" src="${thumbnail_url}" alt="" />
         <div class="p-6 flex flex-col justify-start">
-          <h5 class="text-gray-900 text-xl font-medium mb-2"></h5>
-          <p class="text-gray-700 text-base mb-4"></p>
+          <h5 class="text-gray-900 text-xl font-medium mb-2">${title}
+          </h5>
+          <p class="text-gray-700 text-base mb-4">${details.slice(0,150)}...</p>
           
           
          
