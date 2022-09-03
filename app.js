e4c94bce1dfd09c8609bda31613e040da8628033
Joy <mmehedihasanjoyv@gmail.com>
2022-09-03 22:27:28 +0600

    added details button

--- app.js
+++ app.js
@@ -58,12 +58,14 @@ const displayClickedItem = (categories) => {
         <div class="p-6 flex flex-col justify-start">
           <h5 class="text-gray-900 text-xl font-medium mb-2">${title}
           </h5>
-          <p class="text-gray-700 text-base mb-4">${details.slice(0, 150)}...</p>        
- <div class="text-sm inline-grid grid-cols-2 gap-2">
+          <p class="text-gray-700 text-base mb-4">${details.slice(0, 150)}...</p>   
+          <button type="button" class="inline-block px-2 py-2.5 bg-gray-200 text-gray-700 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out">Details...</button>     
+ <div class="text-sm inline-grid lg:grid-cols-3 sm:grid-cols-2 gap-2">
  <div><img class="w-10 h-10 inline-grid  rounded-full " src="${author.img}">
  <span class="font-semibold">${author.name ? author.name : 'no data available'}</span></div>
- <div><p class="font-semibold mt-2">View: ${total_view ? total_view : 'no data available'}</p></div>
-         
+ <div><p class="font-semibold mt-2">View: ${total_view ? total_view : 'no data available'}</p> </div>
+ 
+ 
       </div>
          
         </div>
